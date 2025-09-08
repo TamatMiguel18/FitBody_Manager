e28fc19290ff1c9e1ff9a511eece53b214cdbd0c
Miguel Tamat <mtamat-2024024@kinal.edu.gt>
2025-09-07 22:29:58 -0600

    primer commit

---
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/FitBodyManagerApplication.java
@@ -0,0 +1,13 @@
+package org.dev_busters.FitBody_Manager.FitBody.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitBodyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitBodyManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dev_busters/FitBody_Manager/FitBody/Manager/FitBodyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev_busters.FitBody_Manager.FitBody.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitBodyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
