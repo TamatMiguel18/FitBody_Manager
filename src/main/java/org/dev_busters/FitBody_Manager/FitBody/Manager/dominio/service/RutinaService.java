2aa452667aa39d99b0f61e4270cdcee655d39abe
Miguel Tamat <mtamat-2024024@kinal.edu.gt>
2025-09-12 10:02:35 -0600

    Se agreggo el archivo service de Rutina

--- src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/service/RutinaService.java
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/service/RutinaService.java
@@ -1,4 +1,29 @@
 package org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.service;
 
+import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.dto.RutinaDto;
+import org.dev_busters.FitBody_Manager.FitBody.Manager.repository.RutinaRepository;
+import org.springframework.stereotype.*;
+
+import java.util.List;
+
+@Service
 public class RutinaService {
+    private final RutinaRepository rutinaRepository;
+
+    public RutinaService (RutinaRepository rutinaRepository){
+        this.rutinaRepository = rutinaRepository;
+    }
+
+    public List<RutinaDto> obtenerTodo(){
+        return this.rutinaRepository.obtenerTodo();
+    }
+
+    public RutinaDto buscarPorCodigo (Long idRutina){
+        return this.rutinaRepository.buscarPorCodigo(idRutina);
+    }
+
+    public RutinaDto guardarRutina(RutinaDto rutinaDto){
+        return this.rutinaRepository.guardarRutina(rutinaDto);
+    }
+
 }
