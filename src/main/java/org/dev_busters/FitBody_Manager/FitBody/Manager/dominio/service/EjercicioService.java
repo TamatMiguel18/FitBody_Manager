c5e24c7ff38533106e30c121d7d7c63246d5d638
Miguel Tamat <mtamat-2024024@kinal.edu.gt>
2025-09-12 10:01:37 -0600

    Se agreggo el archivo service de Ejercicio

--- src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/service/EjercicioService.java
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/service/EjercicioService.java
@@ -1,4 +1,29 @@
 package org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.service;
 
+import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.dto.EjercicioDto;
+import org.dev_busters.FitBody_Manager.FitBody.Manager.repository.EjercicioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EjercicioService {
+    private final EjercicioRepository ejercicioRepository;
+
+    public EjercicioService (EjercicioRepository ejercicioRepository){
+        this.ejercicioRepository = ejercicioRepository;
+    }
+
+    public List<EjercicioDto> obtenerTodo(){
+        return this.ejercicioRepository.obtenerTodo();
+    }
+
+    public EjercicioDto buscarPorCodigo (Long idEjercicio){
+        return this.ejercicioRepository.buscarPorCodigo(idEjercicio);
+    }
+
+    public EjercicioDto guardarRutina(EjercicioDto ejercicioDto){
+        return this.ejercicioRepository.guardarEjercicio(ejercicioDto);
+    }
+
 }
