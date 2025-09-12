198e091521b1ac9b40ccb59caa4921c62a4a4e36
Miguel Tamat <mtamat-2024024@kinal.edu.gt>
2025-09-12 10:08:13 -0600

    se agrego RutinaRepository

--- src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/repository/RutinaRepository.java
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/repository/RutinaRepository.java
@@ -1,4 +1,16 @@
-package org.dev_busters.FitBody_Manager.FitBody.Manager.repository;
+    package org.dev_busters.FitBody_Manager.FitBody.Manager.repository;
 
-public interface RutinaRepository {
-}
+    import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.dto.RutinaDto;
+    import org.dev_busters.FitBody_Manager.FitBody.Manager.persistence.crud.CrudRutinaEntity;
+    import org.springframework.stereotype.Repository;
+
+    import java.util.List;
+
+    @Repository
+    public interface RutinaRepository{
+
+        List<RutinaDto> obtenerTodo();
+        RutinaDto buscarPorCodigo(Long idRutina);
+        RutinaDto guardarRutina(RutinaDto rutinaDto);
+        RutinaDto modificarRutina(Long idRutina);
+    }
