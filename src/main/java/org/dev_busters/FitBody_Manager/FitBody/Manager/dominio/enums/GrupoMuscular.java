882b48250be02adb755fa174b079cc7fecb1c160
Miguel Tamat <mtamat-2024024@kinal.edu.gt>
2025-09-12 09:59:55 -0600

    Se agrego el archivo Enum de Grupo Muscular

--- src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/enums/GrupoMuscular.java
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/enums/GrupoMuscular.java
@@ -1,4 +1,10 @@
 package org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.enums;
 
 public enum GrupoMuscular {
+    Piernas,
+    Pecho,
+    Espalda,
+    Brazos,
+    Hombros,
+    Abdomen
 }
