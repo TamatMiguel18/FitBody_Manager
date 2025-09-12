9a4ec20ece946265853843fb7dec560d5bd36965
Miguel Tamat <mtamat-2024024@kinal.edu.gt>
2025-09-12 10:03:43 -0600

    Se agrego el CrudEntity de Ejercicio

--- src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/persistence/crud/CrudEjercicioEntity.java
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/persistence/crud/CrudEjercicioEntity.java
@@ -1,4 +1,7 @@
 package org.dev_busters.FitBody_Manager.FitBody.Manager.persistence.crud;
 
-public interface CrudEjercicioEntity {
+import org.dev_busters.FitBody_Manager.FitBody.Manager.persistence.entity.EjercicioEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CrudEjercicioEntity extends CrudRepository<EjercicioEntity, Long> {
 }
