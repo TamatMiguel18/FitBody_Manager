63d8261a940d5d5702a30789e2c833504fac9c3c
Miguel Tamat <mtamat-2024024@kinal.edu.gt>
2025-09-12 09:09:33 -0600

    Se agrego el archivo Dto de Rutina

--- src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/dto/RutinaDto.java
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/dto/RutinaDto.java
@@ -1,4 +1,16 @@
 package org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.dto;
 
-public record RutinaDto() {
+import jakarta.validation.constraints.NotNull;
+import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.enums.ClasificacionImc;
+import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.enums.Dificultad;
+import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.enums.Duracion;
+
+public record RutinaDto(
+        Long idRutina,
+        String nombreRutina,
+        Dificultad dificultad,
+        Duracion duracion,
+        Integer frecuencia,
+        ClasificacionImc clasificacionImc
+) {
 }
