e017a4e0d0497939bf53a6d8f8021789259961eb
Miguel Tamat <mtamat-2024024@kinal.edu.gt>
2025-09-12 10:07:47 -0600

    se agrego EjercicioRepository

--- src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/repository/EjercicioRepository.java
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/repository/EjercicioRepository.java
@@ -1,4 +1,16 @@
 package org.dev_busters.FitBody_Manager.FitBody.Manager.repository;
 
-public interface EjercicioRepository {
+import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.dto.EjercicioDto;
+import org.dev_busters.FitBody_Manager.FitBody.Manager.persistence.crud.CrudEjercicioEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EjercicioRepository{
+
+    List<EjercicioDto> obtenerTodo();
+    EjercicioDto buscarPorCodigo(Long idEjercicio);
+    EjercicioDto guardarEjercicio(EjercicioDto ejercicioDto);
+    EjercicioDto modificarEjercicio(Long idEjercicio);
 }
