da6a5e1ee43b6259e8ffc64fe3fadd300eca3dd7
D1eegoMUI <dlopez-2021700@kinal.edu.gt>
2025-09-16 10:53:06 -0600

    Se crearon 2 excepciones para el usuario, pero solamente las clases, aun no se agrega nada

---
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/exception/UsuarioNoExisteException.java
@@ -0,0 +1,7 @@
+package org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.exception;
+
+public class UsuarioNoExisteException extends RuntimeException {
+    public UsuarioNoExisteException(String message) {
+        super(message);
+    }
+}
