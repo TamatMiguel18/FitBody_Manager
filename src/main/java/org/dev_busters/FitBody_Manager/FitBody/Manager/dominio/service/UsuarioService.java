af199dff739b3950cc4064f0fb62e3067c5ebf89
D1eegoMUI <dlopez-2021700@kinal.edu.gt>
2025-09-16 10:44:58 -0600

    Se modifico el Servicio de usuario: UsuarioService

--- src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/service/UsuarioService.java
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/service/UsuarioService.java
@@ -1,4 +1,33 @@
 package org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.service;
 
+import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.dto.UsuarioDto;
+import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.dto.ModUsuarioDto;
+import org.dev_busters.FitBody_Manager.FitBody.Manager.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository){ this.usuarioRepository = usuarioRepository;}
+
+    public List<UsuarioDto> obtenerTodos(){ return this.usuarioRepository.obtenerTodos();}
+
+    public UsuarioDto buscarPorId(Long Id){
+        return this.usuarioRepository.buscarPorId(Id);
+    }
+
+    public UsuarioDto guardarUsuario(UsuarioDto usuarioDto){
+        return this.usuarioRepository.guardarUsuario(usuarioDto);
+    }
+
+    public UsuarioDto modificarUsuario(Long Id, ModUsuarioDto modUsuarioDto){
+        return this.usuarioRepository.modificarUsuario(Id, modUsuarioDto);
+    }
+
+    public void eliminarUsuario(Long Id){
+        this.usuarioRepository.eliminarUsuario(Id);
+    }
 }
