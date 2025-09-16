62014c4d5916ef1213f42ab0f28ec5765cf1d2ab
D1eegoMUI <dlopez-2021700@kinal.edu.gt>
2025-09-16 10:26:54 -0600

    Se modifico la entidad de Usuarios

--- src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/persistence/entity/UsuarioEntity.java
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/persistence/entity/UsuarioEntity.java
@@ -1,4 +1,40 @@
 package org.dev_busters.FitBody_Manager.FitBody.Manager.persistence.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "Usuarios")
+@Data
 public class UsuarioEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+    @Column(length = 64, nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private Integer edad;
+
+    @Column(nullable = false)
+    private String sexo;
+
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal altura;
+
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal peso;
+
+    @Column(name = "peso_deseado", precision = 10, scale = 2, nullable = false)
+    private BigDecimal pesoDeseado;
+
+    @Column(name = "clasificacion_imc", nullable = false)
+    private String clasificacionImc;
+
+    @Column(name = "objetivo_personal", nullable = false)
+    private String objetivoPersonal;
+    
+    @Column(name = "masa_corporal", precision = 10, scale = 2, nullable = false)
+    private BigDecimal masaCorporal;
 }
