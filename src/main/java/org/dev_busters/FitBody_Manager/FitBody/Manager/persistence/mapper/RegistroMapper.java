85cbdbe84a2a0c5280c7899faf5894b5666a1621
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2025-09-16 11:46:44 -0600

    Se creo el mapper de Registro

---
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/persistence/mapper/RegistroMapper.java
@@ -0,0 +1,18 @@
+package org.dev_busters.FitBody_Manager.FitBody.Manager.persistence.mapper;
+
+import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.dto.ModRegistroDto;
+import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.dto.RegistroDto;
+import org.dev_busters.FitBody_Manager.FitBody.Manager.persistence.entity.RegistroEntity;
+import org.dev_busters.FitBody_Manager.FitBody.Manager.persistence.entity.UsuarioEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface RegistroMapper {
+    RegistroDto toDto(RegistroEntity registroEntity);
+    List<RegistroDto> toDto(Iterable<RegistroEntity> entities);
+    RegistroEntity toEntity(RegistroDto usuarioRegistroDto);
+    void modificarEntityFromDto(ModRegistroDto modRegistroDto, @MappingTarget RegistroEntity registroEntity);
+}
