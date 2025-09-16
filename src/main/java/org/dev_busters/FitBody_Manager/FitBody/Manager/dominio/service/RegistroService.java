bc0d87650285a148ea25487ac8f862092061613a
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2025-09-16 11:46:31 -0600

    Se creo el servicio de Registro

---
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/service/RegistroService.java
@@ -0,0 +1,37 @@
+package org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.service;
+
+import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.dto.RegistroDto;
+import org.dev_busters.FitBody_Manager.FitBody.Manager.repository.RegistroRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RegistroService {
+
+    private final RegistroRepository registroRepository;
+
+    public RegistroService(RegistroRepository registroRepository) {
+        this.registroRepository = registroRepository;
+    }
+
+    public List<RegistroDto> listarRegistros(){
+        return this.registroRepository.obtenerTodo();
+    }
+
+    public RegistroDto buscarRegistroPorId(Long idRegistro){
+        return this.registroRepository.buscarPorId(idRegistro);
+    }
+
+    public RegistroDto guardarRegistro(RegistroDto registroDto){
+        return this.registroRepository.guardarRegistro(registroDto);
+    }
+
+    public RegistroDto modificarRegistro(Long idRegistro, RegistroDto registroDto){
+        return this.registroRepository.modificarRegistro(idRegistro, registroDto);
+    }
+
+    public void eliminarRegistro(Long idRegistro){
+        this.registroRepository.eliminarRegistro(idRegistro);
+    }
+}
