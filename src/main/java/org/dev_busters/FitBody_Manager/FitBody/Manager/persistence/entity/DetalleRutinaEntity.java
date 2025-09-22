5bb995a6e8da36234b38bfe691e27d5684f196a7
D1eegoMUI <dlopez-2021700@kinal.edu.gt>
2025-09-22 15:03:19 -0600

    Se modifico el entity de detalleRutina

--- src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/persistence/entity/DetalleRutinaEntity.java
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/persistence/entity/DetalleRutinaEntity.java
@@ -1,40 +1,25 @@
 package org.dev_busters.FitBody_Manager.FitBody.Manager.persistence.entity;
-//import org.dev_busters.FitBody_Manager.FitBody.Manager.persistence.entity.EjercicioEntity;
-//import org.dev_busters.FitBody_Manager.FitBody.Manager.persistence.entity.RutinaEntity;
+
 import jakarta.persistence.*;
 import lombok.Data;
+import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.enums.Clasificacion;
+import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.enums.Dificultad;
+import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.enums.Duracion;
 
-//@Entity
-//@Table(name = "DetalleRutina")
-//@Data
+@Entity
+@Table(name = "DetalleRutina")
+@Data
 public class DetalleRutinaEntity {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer idDetalleRutina;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "idEjercicio", nullable = false)
-//    private EjercicioEntity ejercicio;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "idRutina", nullable = false)
-//    private RutinaEntity rutina;
-//
-//    @Column(length = 64, nullable = false)
-//    private String nombreRutina;
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(nullable = false)
-//    private Dificultad dificultad;
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(nullable = false)
-//    private Duracion duracion;
-//
-//    private Integer frecuencia;
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(name = "clasificacionImc", nullable = false)
-//    private ClasificacionImc clasificacionImc;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idDetalleRutina;
+
+    @ManyToOne
+    @JoinColumn(name = "idEjercicio", referencedColumnName = "idEjercicio", nullable = false)
+    private EjercicioEntity ejercicio;
+
+    @ManyToOne
+    @JoinColumn(name = "idRutina", referencedColumnName = "idRutina", nullable = false)
+    private RutinaEntity rutina;
 }
