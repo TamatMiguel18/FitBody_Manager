487a855c7d4121e9c86562eeae774196bbe73e98
D1eegoMUI <dlopez-2021700@kinal.edu.gt>
2025-09-23 16:54:28 -0600

    Se modificaron los Dto de DetalleRutina para la implementacionde jsf

--- src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/dto/DetalleRutinaDto.java
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/dto/DetalleRutinaDto.java
@@ -4,37 +4,41 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.enums.Clasificacion;
 import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.enums.Dificultad;
 import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.enums.Duracion;
 
-public record DetalleRutinaDto(
-        Long idDetalleRutina,
+@Data
+@AllArgsConstructor
+public class DetalleRutinaDto{
+    Long idDetalleRutina;
 
-        @NotNull(message = "El id del ejercicio es obligatorio")
-        @Positive(message = "El id del ejercicio debe ser un número positivo")
-        Integer idEjercicio,
+    @NotNull(message = "El id del ejercicio es obligatorio")
+    @Positive(message = "El id del ejercicio debe ser un número positivo")
+    Integer idEjercicio;
 
-        @NotNull(message = "El id de la rutina es obligatorio")
-        @Positive(message = "El id de la rutina debe ser un número positivo")
-        Integer idRutina,
+    @NotNull(message = "El id de la rutina es obligatorio")
+    @Positive(message = "El id de la rutina debe ser un número positivo")
+    Integer idRutina;
 
-        @NotBlank(message = "El nombre de la rutina es obligatorio")
-        String nombreRutina,
+    @NotBlank(message = "El nombre de la rutina es obligatorio")
+    String nombreRutina;
 
-        @Valid
-        @NotNull(message = "La dificultad es obligatoria")
-        Dificultad dificultad,
+    @Valid
+    @NotNull(message = "La dificultad es obligatoria")
+    Dificultad dificultad;
 
-        @Valid
-        @NotNull(message = "La duración es obligatoria")
-        Duracion duracion,
+    @Valid
+    @NotNull(message = "La duración es obligatoria")
+    Duracion duracion;
 
-        @NotNull(message = "La frecuencia es obligatoria")
-        @Positive(message = "La frecuencia debe ser un número positivo")
-        Integer frecuencia,
+    @NotNull(message = "La frecuencia es obligatoria")
+    @Positive(message = "La frecuencia debe ser un número positivo")
+    Integer frecuencia;
 
-        @Valid
-        @NotNull(message = "La clasificación IMC es obligatoria")
-        Clasificacion clasificacionImc
-) {}
+    @Valid
+    @NotNull(message = "La clasificación IMC es obligatoria")
+    Clasificacion clasificacionImc;
+}
--- src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/dto/ModDetalleRutinaDto.java
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/dto/ModDetalleRutinaDto.java
@@ -3,24 +3,28 @@ package org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.enums.Clasificacion;
 import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.enums.Dificultad;
 import org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.enums.Duracion;
 
-public record ModDetalleRutinaDto(
-        @NotBlank(message = "El nombre de la rutina es obligatorio")
-        String nombreRutina,
+@Data
+@AllArgsConstructor
+public class ModDetalleRutinaDto{
+    @NotBlank(message = "El nombre de la rutina es obligatorio")
+    String nombreRutina;
 
-        @NotNull(message = "La dificultad es obligatoria")
-        Dificultad dificultad,
+    @NotNull(message = "La dificultad es obligatoria")
+    Dificultad dificultad;
 
-        @NotNull(message = "La duración es obligatoria")
-        Duracion duracion,
+    @NotNull(message = "La duración es obligatoria")
+    Duracion duracion;
 
-        @NotNull(message = "La frecuencia es obligatoria")
-        @Positive(message = "La frecuencia debe ser un número positivo")
-        Integer frecuencia,
+    @NotNull(message = "La frecuencia es obligatoria")
+    @Positive(message = "La frecuencia debe ser un número positivo")
+    Integer frecuencia;
 
-        @NotNull(message = "La clasificación IMC es obligatoria")
-        Clasificacion clasificacionImc
-) {}
+    @NotNull(message = "La clasificación IMC es obligatoria")
+    Clasificacion clasificacionImc;
+}
