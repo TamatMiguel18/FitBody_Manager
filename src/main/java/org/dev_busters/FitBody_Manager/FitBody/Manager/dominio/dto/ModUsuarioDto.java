fc5c150da223f4f458a5bd5f842ee205aed30ee9
D1eegoMUI <dlopez-2021700@kinal.edu.gt>
2025-09-23 16:52:36 -0600

    Se modifico el ModDto de usuario para la implementacion de jsf

--- src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/dto/ModUsuarioDto.java
+++ src/main/java/org/dev_busters/FitBody_Manager/FitBody/Manager/dominio/dto/ModUsuarioDto.java
@@ -1,18 +1,23 @@
 package org.dev_busters.FitBody_Manager.FitBody.Manager.dominio.dto;
 
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.math.BigDecimal;
 
-public record ModUsuarioDto(
-        @DecimalMin(value = "0.01", message = "La altura debe ser mayor que 0")
-        BigDecimal altura,
+@Data
+@AllArgsConstructor
+public class ModUsuarioDto {
+    @DecimalMin(value = "0.01", message = "La altura debe ser mayor que 0")
+    BigDecimal altura;
 
-        @DecimalMin(value = "0.01", message = "El peso debe ser mayor que 0")
-        BigDecimal peso,
+    @DecimalMin(value = "0.01", message = "El peso debe ser mayor que 0")
+    BigDecimal peso;
 
-        @DecimalMin(value = "0.01", message = "El peso deseado debe ser mayor que 0")
-        BigDecimal pesoDeseado,
+    @DecimalMin(value = "0.01", message = "El peso deseado debe ser mayor que 0")
+    BigDecimal pesoDeseado;
 
-        @NotBlank(message = "El objetivo personal es obligatorio")
-        String objetivoPersonal
-) {}
+    @NotBlank(message = "El objetivo personal es obligatorio")
+    String objetivoPersonal;
+}
